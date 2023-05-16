8201ce92503c97a4836b0c2208005e6de603367e
huangdianhua <huangdianhua@yiqicai.com>
2023-05-16 15:10:57 +0800

    配置文件格式

--- src/main/kotlin/com/github/openjkdev/picturezipplugin/bean/ImagePathBean.kt
+++ src/main/kotlin/com/github/openjkdev/picturezipplugin/bean/ImagePathBean.kt
@@ -5,9 +5,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ImagePathBean(var name: String?, var path: String?, var select: Boolean) {
     override fun toString(): String {
+
         return """{
             |"name":"$name", 
-            |"path":"$path", 
+            |"path":"${path?.replace("\\", "\\\\")}", 
             |"select":"$select"
             |}""".trimMargin()
     }
